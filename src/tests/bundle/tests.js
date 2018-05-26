6dfc469e5d09e582cec75bfd64799f202e65bf6a
minhdao <minhdao6@gmail.com>
2018-05-26 14:51:28 +0700

    setup mocha to work with webpack

---
+++ src/tests/bundle/tests.js
@@ -0,0 +1,158 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = "./src/tests/utilities.test.js");
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./src/tests/utilities.test.js":
+/*!*************************************!*\
+  !*** ./src/tests/utilities.test.js ***!
+  \*************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+var utilities = __webpack_require__(/*! ./../utilities.js */ "./src/utilities.js");
+
+describe('utilities.js add()', function () {
+  it('should return correct sum of two positive numbers', function () {
+    var x = 12;
+    var y = 43;
+    var expectedResult = 55;
+    var result = utilities.add(12, 43);
+    if (result !== expectedResult) {
+      throw new Error('expected ' + expectedResult + ' but got ' + result);
+    }
+  });
+
+  it('should return correct sum of two negative numbers', function () {
+    var x = -10;
+    var y = -32;
+    var expectedResult = -42;
+    var result = utilities.add(x, y);
+    if (result !== expectedResult) {
+      throw new Error('expected ' + expectedResult + ' but got ' + result);
+    }
+  });
+
+  it('should return correct sum of one negative and one positive number', function () {
+    var x = 13;
+    var y = -32;
+    var expectedResult = -19;
+    var result = utilities.add(x, y);
+    if (result !== expectedResult) {
+      throw new Error('expected ' + expectedResult + ' but got ' + result);
+    }
+  });
+
+  it('should reject and display error message if either input is not number', function () {
+    var x = 'a';
+    var y = 12;
+    var expectedResult = 'can only add numbers.';
+    var result = utilities.add(x, y);
+    if (result !== expectedResult) {
+      throw new Error('expected ' + expectedResult + ' but got ' + result);
+    }
+  });
+});
+
+/***/ }),
+
+/***/ "./src/utilities.js":
+/*!**************************!*\
+  !*** ./src/utilities.js ***!
+  \**************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+/**
+ * [add return the sum of 2 numbers]
+ * @param {[number]} x [first number]
+ * @param {[number]} y [second number]
+ */
+var add = exports.add = function add(x, y) {
+  if (isNaN(x) || isNaN(y)) {
+    return 'can only add numbers.';
+  }
+  return x + y;
+};
+
+/***/ })
+
+/******/ });
+//# sourceMappingURL=tests.js.map
---
+++ src/tests/utilities.test.js
@@ -0,0 +1,43 @@
+const utilities = require('./../utilities.js');
+
+describe('utilities.js add()', () => {
+  it('should return correct sum of two positive numbers', () => {
+    let x = 12;
+    let y = 43;
+    let expectedResult = 55;
+    let result = utilities.add(12, 43);
+    if (result !== expectedResult) {
+      throw new Error(`expected ${expectedResult} but got ${result}`);
+    }
+  });
+
+  it('should return correct sum of two negative numbers', () => {
+    let x = -10;
+    let y = -32;
+    let expectedResult = -42;
+    let result = utilities.add(x, y);
+    if (result !== expectedResult) {
+      throw new Error(`expected ${expectedResult} but got ${result}`);
+    }
+  });
+
+  it('should return correct sum of one negative and one positive number', () => {
+    let x = 13;
+    let y = -32;
+    let expectedResult = -19;
+    let result = utilities.add(x, y);
+    if (result !== expectedResult) {
+      throw new Error(`expected ${expectedResult} but got ${result}`);
+    }
+  });
+
+  it('should reject and display error message if either input is not number', () => {
+    let x = 'a';
+    let y = 12;
+    let expectedResult = 'can only add numbers.';
+    let result = utilities.add(x, y);
+    if (result !== expectedResult) {
+      throw new Error(`expected ${expectedResult} but got ${result}`);
+    }
+  });
+});
