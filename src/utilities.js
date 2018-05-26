62a5a051980acd9d3c5cdacc5d788604a5d334da
minhdao <minhdao6@gmail.com>
2018-05-26 14:50:55 +0700

    now use isNaN() correctly

--- src/utilities.js
+++ src/utilities.js
@@ -4,7 +4,7 @@
  * @param {[number]} y [second number]
  */
 export const add = (x, y) => {
-  if (x.isNaN || y.isNaN) {
+  if (isNaN(x) || isNaN(y)) {
     return 'can only add numbers.';
   }
   return x + y;
